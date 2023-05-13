206b7370eb88293f24bb656bce24197b916bd78e
kerman <kerman@qq.com>
2023-05-13 14:28:49 +0800

    docs: menu文档编写

--- src/components/Menu/demo/base.tsx
+++ src/components/Menu/demo/base.tsx
@@ -2,11 +2,14 @@ import React from 'react';
 import { Menu } from 'raind';
 const App: React.FC = () => {
   return (
-    <Menu mode="horizontal">
-      <Menu.Item disabled>A</Menu.Item>
-      <Menu.Item>B</Menu.Item>
-      <Menu.Item>C</Menu.Item>
-      <Menu.Item>A</Menu.Item>
+    <Menu>
+      <Menu.Item>cool link</Menu.Item>
+      <Menu.Item>cool link 2</Menu.Item>
+      <Menu.Item disabled>disabled</Menu.Item>
+      <Menu.SubMenu title="下拉选项">
+        <Menu.Item>下拉选项一</Menu.Item>
+        <Menu.Item>下拉选项二</Menu.Item>
+      </Menu.SubMenu>
     </Menu>
   );
 };
